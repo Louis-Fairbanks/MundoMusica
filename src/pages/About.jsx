891163eb97f86e9ec153a16a13f430d868613ce1
Louis-Fairbanks <Louis-Fairbanks>
2023-06-24 00:18:28 -0300

    Finish home page and begin about and client-side

---
+++ src/pages/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About(){
+    return <div>
+    <div><hr/></div>
+        <h1>What is MundoMusica</h1>
+        <h4>MundoMusica allows you to connect with musicians from around the world to put together your 
+        next project. Whether you need the distinct sound of an exotic instrument, the expertise of a maestro
+        to play a piece, or want to put together an album from scratch with different musicians, MundoMusica
+        gives you the tools to do it.</h4>
+        <h1>How it Works</h1>
+        <ol>
+            <li>Sign up. All you need to start is an email, your username, and password</li>
+            <li>Choose what you plan to use the website for.You can advertise your services as a musician, search
+            for musicians for a project, or both.</li>
+            <li>Now you can build your profile. As a musician, you can list what instruments you play,
+            upload samples of your work, describe what genres you are interested in, and set your pricing before advertising your services.
+            If you are searching for musicians, you can describe projects you are working on, what you are looking for,
+            and create listings for the freelancers.</li>
+            <li>At this stage, you're ready to begin browsing for opportunities! When you see a posting, you can apply for it, and as the 
+            poster, you will see applications for your assignments roll in. Once a musician has been selected for a position,
+            they will be able to chat with the coordinator, send samples, and watch a masterpiece materialize!</li>
+        </ol>
+        <h1>About Us</h1>
+        <h4>Lorem Ipsum</h4>
+    </div>
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/Header";
+import Title from "../components/Title";
+
+function Home(){
+    return <div>
+        <Header/>
+        <Title/>
+    </div>
+}
+
+export default Home;
